5c4c00129219f129bb9c174b84641fea9c45cdaf
Hannenur Kıroğlu <hannenur.yazbahar@bil.omu.edu.tr>
2021-06-05 15:06:17 +0300

    Update and rename 30DaysofCode/Day24-MoreLinkedLists.cpp to 30DaysofCode/cpp/Day24-MoreLinkedLists.cpp

--- 30DaysofCode/cpp/Day24-MoreLinkedLists.cpp
+++ 30DaysofCode/cpp/Day24-MoreLinkedLists.cpp
@@ -18,8 +18,7 @@ class Node
 class Solution{
     public:
 
-          Node* removeDuplicates(Node *head)
-          {
+          Node* removeDuplicates(Node *head){
             //Write your code here
             if(head == NULL) return 0;
             Node * cur = head;
@@ -32,46 +31,37 @@ class Solution{
                 {
                     cur = cur->next;    
                 }
-            }
+			}
             return head;
           }
 
-          Node* insert(Node *head,int data)
-          {
+          Node* insert(Node *head,int data){
                Node* p=new Node(data);
                if(head==NULL){
-                   head=p;  
-
+                   head=p;
                }
                else if(head->next==NULL){
                    head->next=p;
-
                }
                else{
                    Node *start=head;
                    while(start->next!=NULL){
                        start=start->next;
                    }
-                   start->next=p;   
-
+                   start->next=p;
                }
-                    return head;
-                
-            
+               return head;
           }
-          void display(Node *head)
-          {
-                  Node *start=head;
-                    while(start)
-                    {
-                        cout<<start->data<<" ";
-                        start=start->next;
-                    }
+          void display(Node *head){
+			  Node *start=head;
+			  while(start){
+				  cout<<start->data<<" ";
+				  start=start->next;
+			  }
            }
 };
 			
-int main()
-{
+int main(){
 	Node* head=NULL;
   	Solution mylist;
     int T,data;
@@ -81,7 +71,5 @@ int main()
         head=mylist.insert(head,data);
     }	
     head=mylist.removeDuplicates(head);
-
 	mylist.display(head);
-		
 }
