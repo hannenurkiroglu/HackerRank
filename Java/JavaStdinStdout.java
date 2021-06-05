dba2b9c999d2c5763b9f1dfcb03031b713f2bd32
Hannenur Kıroğlu <hannenur.yazbahar@bil.omu.edu.tr>
2021-06-05 13:40:58 +0300

    Create JavaStdinStdout.java

---
+++ Java/JavaStdinStdout.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+}
+
+/* 
+One popular way to read input from stdin is by using the Scanner class and specifying the Input Stream as System.in. For example:
+  Scanner scanner = new Scanner(System.in);
+  String myString = scanner.next();
+  int myInt = scanner.nextInt();
+  scanner.close();
+
+  System.out.println("myString is: " + myString);
+  System.out.println("myInt is: " + myInt);
+*/
