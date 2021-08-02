3156108692e70eda4c3513d3f672a0c55a82e7bf
Hannenur Kıroğlu <hannenur.yazbahar@bil.omu.edu.tr>
2021-08-02 22:58:22 +0300

    Create Day03-Arrays.js

---
+++ 10Days0fJavaScript/Day03-Arrays.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let maxNum = nums[0];
+    let secMaxNum = nums[0];
+    
+    for (let el in nums) {
+        if (nums[el] > maxNum){
+            secMaxNum = maxNum;
+            maxNum = nums[el];
+        } else if (nums[el] != maxNum && nums[el] > secMaxNum){
+            secMaxNum = nums[el];
+        }
+    }
+
+    return secMaxNum;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
